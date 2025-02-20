79f538ca58812c733ea888ad2457962abe795f80
Your Name <your-email@example.com>
2025-02-20 18:00:38 +0530

    done

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='no-api-key'
+        apiKey='pwxwsmpu644q8tsfh9sw2t4yhps7yjnbx39mneozuds1jdto'
         
         initialValue={defaultValue}
         init={{
